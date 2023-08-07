0245a82bf489c275333c49b1dc91ba0b5cec0594
smlopez30 <smlopez@gmail.com>
2023-08-07 00:40:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -96,32 +96,34 @@ def productoras_exitosas(productora: str):
     return f'La productora {productora} ha tenido un revenue de {revenue_total} y ha realizado {cantidad_peliculas} películas.'
 
 
-@app.get('/get_director/{director}')
-def get_director(director: str):
+@app.get('/get_director/{nombre_director}')
+def get_director(nombre_director: str):
     # Filtrar el dataset peliculas para obtener solo las películas del director dado
-    director_movies = peliculas[peliculas['director'] == director]
+    director_movies = peliculas[peliculas['director'] == nombre_director]
     
+    if director_movies.empty:
+        raise HTTPException(status_code=404, detail="Director no encontrado")
+
     # Calcular el éxito general del director basado en la suma de revenues / suma de budgets
     total_revenue = director_movies['revenue'].sum()
     total_budget = director_movies['budget'].sum()
     
     if total_budget == 0:
-        director_success = None  # O podrías manejarlo de otra manera si el total del budget es cero
+        director_success = None
     else:
         director_success = total_revenue / total_budget
 
     # Crear una lista de información de cada película del director
     peliculas_info = []
     for _, pelicula in director_movies.iterrows():
-        # Verificar si el costo de la película es cero para evitar la división por cero
         if pelicula['budget'] != 0:
             retorno_individual = pelicula['return']
         else:
-            retorno_individual = 0  # O podrías asignar otro valor predeterminado
+            retorno_individual = None
 
         pelicula_info = {
-            'nombre': pelicula['title'],
-            'fecha_lanzamiento': pelicula['release_date'],
+            'nombre': pelicula.get('title', 'Desconocido'),
+            'fecha_lanzamiento': pelicula.get('release_date', 'Desconocido'),
             'retorno_individual': retorno_individual,
             'costo_pelicula': pelicula['budget'],
             'ganancia_pelicula': pelicula['revenue']
@@ -129,11 +131,10 @@ def get_director(director: str):
         peliculas_info.append(pelicula_info)
 
     return {
-        'director': director,
+        'director': nombre_director,
         'retorno_total_director': director_success,
         'peliculas': peliculas_info
     }
-
     
 @app.get('/recomendacion/{titulo}', response_model=List[str])
 def recomendacion(titulo: str):
